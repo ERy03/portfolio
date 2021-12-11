783a2ad8f12adecd6e83ae542b40389a66e4fa43
Ryota Enoki <ryoenoki2013@gmail.com>
2021-12-11 14:39:03 +0900

    background added

---
+++ source/assets/javascripts/typewriter.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+const type = document.getElementById('type');
+if (type) {
+  new Typed('#type', {
+    strings: ["Check out my projects!"],
+    typeSpeed: 80,
+  });
+};
